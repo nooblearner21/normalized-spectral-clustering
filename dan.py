e831a38a6598f30f8b4c2033d303982e477e90c2
Dan <danmika6425@gmail.com>
2021-02-22 18:24:02 +0200

    added weighted matrix calc function

---
+++ dan.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+#Receive N observations ndarray as argument and returns the
+#weighted adjacency matrix
+def getWeightedMatrix(observations):
+    
+    #Create a zero-value NxN matrix
+    weightedMatrix = np.zeros_like(observations, dtype=np.float64)
+
+    #counters
+    i = 0
+    j = 0
+
+    #Calculate euclidean distance between all nodes and fill node weights in our adjacency matrix
+    for vectorx in observations:
+        for vectory in observations:
+            dist = np.sum((vectorx - vectory) ** 2) / 2
+            weightedMatrix[i,j] = np.exp(-dist)
+
+            j += 1
+        i += 1
+        j = 0
+    
+    #Change the diagonal values to zero since we do not allow edges from a node to itself
+    np.fill_diagonal(weightedMatrix, 0)
+
+    return weightedMatrix
+
+
+x = np.arange(1, 17).reshape(4,4)
+
+
+
+
+def getNormalizedLaplacian(weightedAdjMatrix):
+    
+    #create NxN Identity Matrix I
+    idMatrix = np.identity(weightedAdjMatrix.shape[1])
+    #Retrieve Diagonal degree matrix
+    diagonalMatrix = getDiagonalDegreeMatrix(weightedAdjMatrix)
+
+    #Calculate and return laplacian
+    laplacian = idMatrix - (diagonalMatrix @ weightedAdjMatrix @ diagonalMatrix)
+
+    return laplacian
+    
+getNormalizedLaplacian(getWeightedMatrix(x))
