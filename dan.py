c05d8201639ab4d816f86ecd5bddaabfbb6e0447
Dan <danmika6425@gmail.com>
2021-02-23 19:13:34 +0200

    added qr iterations

--- dan.py
+++ dan.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #Receive N observations ndarray as argument and returns the
 #weighted adjacency matrix
-def getWeightedMatrix(observations):
+def get_weighted_adj_matrix(observations):
     
     #Create a zero-value NxN matrix
     weightedMatrix = np.zeros_like(observations, dtype=np.float64)
@@ -46,3 +46,24 @@ def getNormalizedLaplacian(weightedAdjMatrix):
     return laplacian
     
 getNormalizedLaplacian(getWeightedMatrix(x))
+
+
+
+def qr_iterations(matrix):
+    aroof = np.copy(matrix)
+    qroof = np.identity(matrix.shape[1])
+
+    #change to N
+    for i in range(10000):
+        qr = mgs_algorithm(aroof)
+        
+        aroof = qr[1] @ qr[0]
+
+
+        matrix_distance = np.abs(np.abs(qroof) - (np.abs(qroof @ qr[0])))
+
+        if((matrix_distance > 0.0001).all()):
+            return (aroof, qroof)
+        qroof = qroof @ qr[0]
+    
+    return (aroof, qroof)
