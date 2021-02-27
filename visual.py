dcefdb0ab264cd863c8c01d2b491dc20c6ad63e6
Yonatan Tin <yonitint1@gmail.com>
2021-02-27 15:11:16 +0200

    labled each point its cluster index, an array that we pass to the c att in scatter func

--- visual.py
+++ visual.py
@@ -3,8 +3,27 @@ from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-#need to grab input instead
+#redundent to grab the points themselves??
+#clusters_points = []
 
+#for i in range(k):
+    #curr_cluster_points = [X_1[x] for x in clusters_points_indexes[i]]
+    #clusters_points.append(curr_cluster_points)
+
+#test case of index input
+clusters_points_indexes = [[1, 2], [7, 8, 3], [5, 6], [4, 0, 9]]
+k = len(clusters_points_indexes)
+
+#labeling each to each point its cluster index,change to n afterwards
+labels = np.zeros(shape=10)
+print(labels)
+
+for i in range(k):
+    for vector_index in clusters_points_indexes[i]:
+        labels[vector_index] = i
+
+
+#visual output code, simple test case
 fig = plt.figure()
 
 #two dimensions case
@@ -14,6 +33,7 @@ y_1 = X_1[0:, 1]
 
 ax_1 = fig.add_subplot(211)
 ax_1.scatter(x_1, y_1, c=labels_1, cmap='RdBu')
+
 
 #three dimensions case
 X_2, labels_2 = make_blobs(n_samples=10, n_features=3)
