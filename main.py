2428f30268116b5a9122b07d177fb0b73d211c46
Dan <danmika6425@gmail.com>
2021-02-27 21:55:58 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 from sklearn.datasets import make_blobs
+from scipy.sparse.csgraph import laplacian
 import numpy as np
 
 
@@ -54,18 +55,24 @@ def get_weighted_adj_matrix(observations):
 
 
 
+def get_diagonal_degree_matrix(adj_matrix):
+    diag_values = np.sum(adj_matrix, axis=1)
+    diag_values_normalized = np.power(diag_values, -0.5)
+
+    matrix_d = np.diag(diag_values_normalized)
+
+    return matrix_d
 
 
-
-def getNormalizedLaplacian(weightedAdjMatrix):
+def get_normalized_laplacian(weighted_adj_matrix):
     
     #create NxN Identity Matrix I
-    idMatrix = np.identity(weightedAdjMatrix.shape[1])
+    id_matrix = np.identity(weighted_adj_matrix.shape[1])
     #Retrieve Diagonal degree matrix
-    diagonalMatrix = getDiagonalDegreeMatrix(weightedAdjMatrix)
+    diagonal_matrix = get_diagonal_degree_matrix(weighted_adj_matrix)
 
     #Calculate and return laplacian
-    laplacian = idMatrix - (diagonalMatrix @ weightedAdjMatrix @ diagonalMatrix)
+    laplacian = id_matrix - (diagonal_matrix @ weighted_adj_matrix @ diagonal_matrix)
 
     return laplacian
 
@@ -89,6 +96,74 @@ def qr_iterations(matrix):
     
     return (aroof, qroof)
 
+def mgs_algorithm(aroof):
+    n = len(aroof)
+    q_matrix = np.zeros(shape=(n, n))
+    r_matrix = np.zeros(shape=(n, n))
+
+    for i in range(n):
+        column_i_norm = LA.norm(aroof, axis=0)[i]
+
+        r_matrix[i, i] = np.power(column_i_norm, 2)
+        q_matrix[:, i] = np.divide(aroof[:, i], column_i_norm)
+
+        for j in range(i + 1, n):
+            r_matrix[i, j] = q_matrix.transpose()[:, i] @ aroof[:, j]
+            aroof[:, j] = aroof[:, j] - (r_matrix[i, j] * q_matrix[:, i])
+
+    return (q_matrix, r_matrix)
+
+
+def eigengap_heuristic(arr):
+    n = len(arr)
+    sorted_arr = sorted(arr)
+    k = 0
+    max_gap = 0
+
+    for i in range(0, int(np.floor(n / 2))):
+        curr_gap = np.abs(sorted_arr[i] - sorted_arr[i + 1])
+
+        if max_gap < curr_gap:
+            max_gap = curr_gap
+            k = i
+
+    return k
+
+
+def build_u_matrix(matrix_tuple):
+    n = len(matrix_tuple[0])
+    eigenvalues = matrix_tuple[0].diagonal().copy()
+    eigenvectors = matrix_tuple[1].transpose().copy()
+    k = eigengap_heuristic(eigenvalues) + 1
+
+    eigen_map = []
+
+    for i in range(n):
+        eigen_map.append({eigenvalues[i]: eigenvectors[i]})
+
+    sorted_map = sorted(eigen_map, key=lambda item: list(item.keys())[0])
+
+    u_matrix = np.ndarray(shape=(n, k))
+
+    for i in range(k):
+        vector = list(sorted_map[i].values())[0]
+        u_matrix[:, i] = vector
+
+    return u_matrix
+
+
+def build_t_matrix(matrix_tuple):
+    u_matrix = build_u_matrix(matrix_tuple)
+
+    n = len(u_matrix)
+    k = len(u_matrix[0])
+    t_matrix = np.ndarray(shape=(n, k))
+    u_matrix_rows_norms = LA.norm(u_matrix, axis=1)
+
+    for i in range(n):
+        t_matrix[i] = np.divide(u_matrix[i], u_matrix_rows_norms[i])
+
+    return t_matrix
 
 
 
@@ -131,4 +206,10 @@ def output_data(index_list, cluster_num):
 if __name__ == '__main__':
     print(x)
     y = get_weighted_adj_matrix(x)
-    print(y)
+    z = get_normalized_laplacian(y)
+
+    q = qr_iterations(z)
+    u = build_u_matrix(r[0])
+    t = build_t_matrix(u)
+    k = eigengap_heuristic()
+    kmeanspp.calc(N, d, x, centroids.tolist(), k, 300)
