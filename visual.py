434bbf47c4fdf16d29a710c26327abc138e883e7
Yonatan Tin <yonitint1@gmail.com>
2021-04-01 18:14:50 +0300

    refactored visual file

--- visual.py
+++ visual.py
@@ -1,52 +1,51 @@
 import numpy as np
-from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-#redundent to grab the points themselves??
-#clusters_points = []
-
-#for i in range(k):
-    #curr_cluster_points = [X_1[x] for x in clusters_points_indexes[i]]
-    #clusters_points.append(curr_cluster_points)
 
 #test case of index input
 clusters_points_indexes = [[1, 2], [7, 8, 3], [5, 6], [4, 0, 9]]
 k = len(clusters_points_indexes)
 
+
 #labeling each to each point its cluster index,change to n afterwards
-labels = np.zeros(shape=10)
-print(labels)
+def build_labels(n, clusters_array):
+    #change to N
+    labels = np.zeros(n)
 
-for i in range(k):
-    for vector_index in clusters_points_indexes[i]:
-        labels[vector_index] = i
+    for cluster in range(len(clusters_array)):
+        for vector_index in clusters_array[cluster]:
+            labels[vector_index] = cluster
 
-
-#visual output code, simple test case
-fig = plt.figure()
 
 #two dimensions case
-X_1, labels_1 = make_blobs(n_samples=10, n_features=2)
-x_1 = X_1[0:, 0]
-y_1 = X_1[0:, 1]
+def visual_2d(observations, labels):
+    fig = plt.figure()
 
-ax_1 = fig.add_subplot(211)
-ax_1.scatter(x_1, y_1, c=labels_1, cmap='RdBu')
+    #X_1, labels_1 = make_blobs(n_samples=10, n_features=2)
+    x = observations[0:, 0]
+    y = observations[0:, 1]
 
+    ax_1 = fig.add_subplot(211)
+    ax_1.scatter(x, y, c=labels, cmap='RdBu')
+
+    fig.suptitle("Project by Dan and Yoni")
+
+    plt.show()
 
 #three dimensions case
-X_2, labels_2 = make_blobs(n_samples=10, n_features=3)
-x_2 = X_2[0:, 0]
-y_2 = X_2[0:, 1]
-z_2 = X_2[0:, 2]
+def visual_3d(observations, labels):
+    fig = plt.figure()
 
-ax_2 = fig.add_subplot(212, projection='3d')
-ax_2.scatter(x_2, y_2, z_2, c=labels_2, cmap="RdBu")
+    #X_2, labels_2 = make_blobs(n_samples=10, n_features=3)
+    x = observations[0:, 0]
+    y = observations[0:, 1]
+    z = observations[0:, 2]
 
-fig.suptitle("Project by Dan and Yoni")
+    ax_2 = fig.add_subplot(212, projection='3d')
+    ax_2.scatter(x, y, z, c=labels, cmap="RdBu")
 
-plt.show()
+    fig.suptitle("Project by Dan and Yoni")
+
+    plt.show()
 
 
 
