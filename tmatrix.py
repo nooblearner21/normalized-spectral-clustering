094307fb99e1334d620e506d1f057047ffdd036d
Yonatan Tin <yonitint1@gmail.com>
2021-04-17 18:11:28 +0300

    replaced for loop in mgs and for loop in t matrix, in numpy functions for efficiency.

--- tmatrix.py
+++ tmatrix.py
@@ -6,12 +6,13 @@ def qr_iterations(matrix):
     aroof = np.copy(matrix)
     qroof = np.identity(matrix.shape[1])
 
+    N = len(matrix)
     # change to N
-    for i in range(10000):
+    for i in range(N):
         qr = mgs_algorithm(aroof)
 
         aroof = qr[1] @ qr[0]
-
+        print(qroof @ qr[0])
         matrix_distance = np.abs(np.abs(qroof) - (np.abs(qroof @ qr[0])))
 
         if ((matrix_distance < 0.0001).all()):
@@ -22,6 +23,12 @@ def qr_iterations(matrix):
 
 
 def mgs_algorithm(aroof):
+    # avoiding rounded values because of int ndarray
+    aroof = aroof.astype(float)
+
+    # adding identity matrix to aroof to avoid columns with norm 0
+    # aroof = aroof + np.identity(aroof.shape[0])
+
     n = len(aroof)
     q_matrix = np.zeros(shape=(n, n))
     r_matrix = np.zeros(shape=(n, n))
@@ -37,11 +44,14 @@ def mgs_algorithm(aroof):
         else:
             raise Exception("norm is 0, so we quit the program")
 
-        for j in range(i + 1, n):
-            #q_i_column = q_matrix[:, i]
-            #aroof_j_column = aroof[:, j]
-            r_matrix[i, j] = q_matrix[:, i] @ aroof[:, j]
-            aroof[:, j] = aroof[:, j] - (r_matrix[i, j] * q_matrix[:, i])
+        q_i_column_transpose = q_matrix[:, i].T
+        aroof_j_columns = np.copy(aroof[:, i + 1:n])
+        r_row_i_j_values = q_i_column_transpose @ aroof_j_columns
+        r_matrix[i, i + 1:n] = r_row_i_j_values
+
+        r_ij_q_i = q_i_column_transpose[:, None] * r_row_i_j_values.reshape(1, r_row_i_j_values.shape[0])
+        aroof_j_new_columns = aroof_j_columns - r_ij_q_i
+        aroof[:, i + 1:n] = aroof_j_new_columns
 
     return (q_matrix, r_matrix)
 
@@ -79,6 +89,7 @@ def build_u_matrix(matrix_tuple):
 
     u_matrix = np.ndarray(shape=(n, k))
 
+
     for i in range(k):
         vector = list(sorted_map[i].values())[0]
         u_matrix[:, i] = vector
@@ -91,14 +102,10 @@ def build_t_matrix(matrix_tuple):
 
     n = len(u_matrix)
     k = len(u_matrix[0])
-    t_matrix = np.ndarray(shape=(n, k))
-    u_matrix_rows_norms = LA.norm(u_matrix, axis=1)
 
-    # change to numpy function instead of for loop to improve performance
-    for i in range(n):
-        row_i_norm = u_matrix_rows_norms[i]
-        if row_i_norm != 0:
-            t_matrix[i] = np.divide(u_matrix[i], u_matrix_rows_norms[i])
+    u_matrix_rows_norms = LA.norm(u_matrix, axis=1)[:, None]
+
+    t_matrix = np.divide(u_matrix, u_matrix_rows_norms, out=np.zeros_like(u_matrix), where=u_matrix_rows_norms != 0)
 
     return t_matrix
 
