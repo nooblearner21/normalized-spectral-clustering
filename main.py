ed114646756ac2e588ddd7472502a9389f2b4de9
Yonatan Tin <yonitint1@gmail.com>
2021-04-25 01:05:31 +0300

    changed tasks to python3.8.5 and returned main to previous commit

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from kmeans_pp import k_means_pp
 parser = argparse.ArgumentParser()
 parser.add_argument("k", type=int)
 parser.add_argument("n", type=int)
-
 parser.add_argument('--random', dest='random', action='store_true')
 parser.add_argument('--no-random', dest='random', action='store_false')
 
@@ -23,72 +22,12 @@ n = args.n
 random = args.random
 d = np.random.randint(2, 4)
 
-
 if random:
     n = np.random.randint(50, 350)
     k = np.random.randint(5, 20)
 
 
 observations, labels = make_blobs(n_samples=n, n_features=d, centers=k)
-
-
-print(random)
-
-"""
-Returns the corresponding cluster index ~ observation relationship
-"""
-def cluster(tmatrix, centroids):
-
-    # create an empty list of K centroids
-    index_list = [[] for i in range(len(centroids))]
-
-
-    #Loop through each row of tmatrix and find the closest centroid
-    #to the coordinates of that row
-    curr_row = -1
-    for row in tmatrix:
-        curr_row += 1
-        min_dist = float("inf")
-        index = 0
-        curr_centroid = -1
-        for centroid in centroids:
-            curr_centroid += 1
-            dist = np.linalg.norm(row - centroid)
-            if (dist < min_dist):
-                min_dist = dist
-                index = curr_centroid
-        index_list[index].append(curr_row)
-
-    return index_list
-
-"""
-Outputs the observations used in this program current run aswell as the labels that were given by the KMeans
-and Spectral Clustering Algorithms
-"""
-def output_data(observations, blob_labels, kmeans_labels, spectral_labels, clusters_num):
-    with open("data.txt", "w") as f:
-        label = 0
-        for observation in observations:
-            for cord in observation:
-                f.write(str(cord) + ",")
-            f.write(str(blob_labels[label]))
-            label += 1
-            f.write("\n")
-    f.close()
-
-    with open("clusters.txt", "w") as f:
-        f.write(str(clusters_num) + "\n")
-        for labels in spectral_labels:
-            f.write(str(labels)[1: -1] + "\n")
-        for labels in kmeans_labels:
-            f.write(str(labels)[1: -1] + "\n")
-    f.close()
-
-k_for_blobs = 5
-#example
-observations, labels = make_blobs(n_samples=50, n_features=3, centers=k_for_blobs)
-
-# Main
 
 laplace_matrix = normalized_laplacian(observations)
 
@@ -121,6 +60,6 @@ spectral_measure, kmeans_measure = visual.jaccard_measure(labels, spectral_label
 
 visual.visual(observations, spectral_labels, kmeans_labels, spectral_measure, kmeans_measure, k, k_for_blobs)
 
-output_data(observations, labels, spectral_clusters_array, kmeans_clusters_array, k)
+output_data(observations, spectral_clusters_array, kmeans_clusters_array, k)
 
 
--- tasks.py
+++ tasks.py
@@ -3,9 +3,9 @@ from invoke import task
 
 @task
 def run(ctx, k=0, n=0, random=True):
-    ctx.run('python setup.py build_ext --inplace')
+    ctx.run('python3.8.5 setup.py build_ext --inplace')
     if(random):
-        ctx.run('python main.py {} {} --random'.format(k, n))
+        ctx.run('python3.8.5 main.py {} {} --random'.format(k, n))
     else:
-        ctx.run('python main.py {} {} --no-random'.format(k, n))
+        ctx.run('python3.8.5 main.py {} {} --no-random'.format(k, n))
 
