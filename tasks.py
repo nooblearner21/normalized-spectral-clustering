63032f3ae8ccee6f93f0a16ae8e1c9f323414771
Yonatan Tin <yonitint1@gmail.com>
2021-04-25 14:36:47 +0300

    updated tasks to Random instead of random

--- tasks.py
+++ tasks.py
@@ -2,10 +2,10 @@ from invoke import task
 
 
 @task
-def run(ctx, k=0, n=0, random=True):
+def run(ctx, k=0, n=0, Random=True):
     ctx.run('python3.8.5 setup.py build_ext --inplace')
-    if(random):
-        ctx.run('python3.8.5 main.py {} {} --random'.format(k, n))
+    if(Random):
+        ctx.run('python3.8.5 main.py {} {} --Random'.format(k, n))
     else:
-        ctx.run('python3.8.5 main.py {} {} --no-random'.format(k, n))
+        ctx.run('python3.8.5 main.py {} {} --no-Random'.format(k, n))
 
